a5a2d12171fb7bc9c69ff20075f70f9b2028040e
dcpelejo <dcpelejo@gmail.com>
2022-05-27 11:45:17 -0400

    Second commit: Updated Assignment to add comments describing the makeCacheMatrix function.

--- cachematrix.R
+++ cachematrix.R
@@ -4,19 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        ## This function returns a list of four functions that will be associated to the input matrix x
+        #if y=CacheMatrix(x), then
+        #y$set sets the value of y to x
+        #y$get returns the value of x
+        #y$setinv saves the inverse of x into the cache
+        #y$getinv returns the inverse of x from the cache (if it's there)
+        
         m <- NULL
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
         get <- function() x
-        setinv <- function(mean) m <<- mean
+        setinv <- function(inv) m <<- inv
         getinv <- function() m
         list(set = set, get = get, setinv = setinv, getinv = getinv) 
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
